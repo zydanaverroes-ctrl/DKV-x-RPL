2622c970c3ff1d7834396603e89833e5394ec744
Dann. <zydanaverroes@gmail.com>
2025-11-24 21:28:19 +0700

    Add files via upload

---
+++ Tailwind Zydan/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.{html,js,ts,jsx,tsx,php,blade.php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
